fdff00df3f8063ff153562e305982f60abf91a4d
Joshua Elliott <joshuaelliott@outlook.com>
2015-03-11 22:50:51 +1100

    Added stream urls to event descriptions and removed bug in incorrect datetime at midnight

--- csgocal.py
+++ csgocal.py
@@ -7,6 +7,7 @@ from apiclient.discovery import build
 from bs4 import BeautifulSoup as bs
 from urllib2 import urlopen, Request
 from datetime import date, timedelta, datetime
+import datetime as dt
 from oauth2client.client import SignedJwtAssertionCredentials
 
 dir = os.path.dirname(__file__)
@@ -58,36 +59,70 @@ class CSGOCalendar:
     
     def update(self):
         url = 'http://www.gosugamers.net/counterstrike/gosubet?u-page=1'
-        request = Request(url, headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'})
+        header = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
+        request = Request(url, headers = header)
         soup = bs(urlopen(request))
         # absolutely unreadable shit. sorry kinda not sorry
+        # we have a few pages to work through
         num_pages = int(soup.find_all(class_='box')[1].find_all('a')[-1]['href'][-1])
         for soup_url in ['http://www.gosugamers.net/counterstrike/gosubet?u-page=' + str(i) for i in xrange(1, num_pages + 1)]:
-            request = Request(soup_url, headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'})
+            request = Request(soup_url, headers = header)
             soup = bs(urlopen(request))
             upcoming_matches = soup.find_all(class_='box')[1]
+            # list comprehensions make me happy
             match_urls = ['http://www.gosugamers.net' + match['href'] for match in upcoming_matches('a', 'match')]
+            # we go through all the upcoming matches that are on the page
             for match_url in match_urls:
-                request = Request(match_url, headers = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'})
+                request = Request(match_url, headers = header)
                 match_soup = bs(urlopen(request))
                 summary_and_league = match_soup.find('h1')
                 summary = summary_and_league.find('label').text.strip()
-                league = summary_and_league.find('a').text.strip()
+                league_info = summary_and_league.find('a')
+
+                league_url = 'http://www.gosugamers.net' + league_info['href']
+                stream_urls = []
+                streams =  []
+                if re.search('/counterstrike/events', league_url):
+                    request = Request(league_url, headers = header)
+                    league_soup = bs(urlopen(request))
+                    for stream_soup in league_soup.find(id='streams').find_all('h3'):
+                        streams.append(stream_soup.text.strip())
+                    for stream_soup in league_soup.find_all(class_='stream-box'):
+                        m = re.search('twitch|hitbox|mlg', str(stream_soup))
+                        if m.group(0) == 'twitch':
+                            stream_urls.append(stream_soup.find('object')['data'])
+                            stream_urls[-1] = stream_urls[-1].replace('widgets/live_embed_player.swf?channel=', '')
+                        elif m.group(0) == 'hitbox':
+                            stream_urls.append(stream_soup.find('iframe')['src'])
+                            stream_urls[-1] = stream_urls[-1].replace('#!/embed/', '')
+                        elif m.group(0) == 'mlg':
+                            stream_urls.append(stream_soup.find('iframe')['src'])
+                            stream_urls[-1] = stream_urls[-1].replace('player/embed/', '').replace('?autoplay=0', '')
+
+                league = league_info.text.strip()
                 best_of_format = match_soup.find(class_='bestof').text.strip()
-                hours = re.search('\d', best_of_format).group(0)
-                datetime = match_soup.find(class_='datetime').text.strip()
-                m = re.match('(?P<month>\w+) (?P<day>\d+), \w+, (?P<start_hour>\d\d):(?P<start_min>\d\d)', datetime)
-                month = self.date_convers[m.groupdict()['month']]
-                day = m.groupdict()['day']
-                time = m.groupdict()['start_hour'] + ':' + m.groupdict()['start_min']
+                length = int(re.search('\d', best_of_format).group(0))
+                datetime_info = match_soup.find(class_='datetime').text.strip()
+                m = re.match('(?P<month>\w+) (?P<day>\d+), \w+, (?P<start_hour>\d\d):(?P<start_min>\d\d)', datetime_info)
+                month = int(self.date_convers[m.groupdict()['month']])
+                day = int(m.groupdict()['day'])
+                start_hour = int(m.groupdict()['start_hour'])
+                start_min = int(m.groupdict()['start_min'])
+                datetime_info = datetime(2015, month, day, start_hour, start_min)
+                stream_info = '-- Streams --\n'
+                for i in xrange(len(streams)):
+                    stream_info += '  ' + streams[i] + ': ' + stream_urls[i] + '\n'
+                if len(streams) == 0:
+                    stream_info = 'A stream was not found for this match'
+
                 event = {
                     'summary' : summary,
-                    'description' : league + ' | ' + best_of_format,
+                    'description' : '-- Event --\n  ' + league + ' | ' + best_of_format + '\n\n' + stream_info,
                     'start' : {
-                        'dateTime' : '2015-' + month + '-' + day + 'T' + time + ':00.000+01:00'
+                        'dateTime' : str(datetime_info.isoformat('T')) + '+01:00'
                     },
                     'end' : {
-                        'dateTime' : '2015-' + month + '-' + day + 'T' + str(int(time[:2])++int(hours)) + time[2:] + ':00.000+01:00'
+                        'dateTime' : str((datetime_info+timedelta(hours=length)).isoformat('T')) + '+01:00'
                     },
                     'source' : {
                         'url' : match_url
@@ -109,21 +144,21 @@ class CSGOCalendar:
         for existing_event in existing_events['items']:
             if existing_event['source']['url'] == event['source']['url']:
                 event_exists = True
-                # if the time is different, update it
-                if not self.sameEventTime(event, existing_event):
+                # if the time is different or description is different, update it
+                if not self.sameEventTime(event, existing_event) or event['description'] != existing_event['description']:
                     updated_event = self.service.events().update(calendarId=self.calendarId, eventId=existing_event['id'], body=event).execute()
                     f.write('*** Updated Event *** ' + str(datetime.now()) + '\n')
                     f.write('Old\n')
-                    f.write(existing_event['summary'] + ' | ' + existing_event['start']['dateTime'] + ' -- ' + existing_event['end']['dateTime'] + ' | ' + existing_event['description'] + '\n')
+                    f.write(existing_event['summary'] + ' | ' + existing_event['start']['dateTime'] + ' -- ' + existing_event['end']['dateTime'] + '\n' + existing_event['description'] + '\n')
                     f.write('New\n')
-                    f.write(updated_event['summary'] + ' | ' + updated_event['start']['dateTime'] + ' -- ' + updated_event['end']['dateTime'] + ' | ' + updated_event['description'] + '\n')
+                    f.write(updated_event['summary'] + ' | ' + updated_event['start']['dateTime'] + ' -- ' + updated_event['end']['dateTime'] + '\n' + updated_event['description'] + '\n')
                     f.write('\n')
                 break
         
         if not event_exists:
             added_event = self.service.events().insert(calendarId=self.calendarId, body=event).execute()
             f.write('*** Added Event *** ' +  str(datetime.now()) + '\n')
-            f.write(added_event['summary'] + ' | ' + added_event['start']['dateTime'] + ' -- ' + added_event['end']['dateTime'] + ' | ' + added_event['description'] + '\n')
+            f.write(added_event['summary'] + ' | ' + added_event['start']['dateTime'] + ' -- ' + added_event['end']['dateTime'] + '\n' + added_event['description'] + '\n')
             f.write('\n')
         f.close()
 
